3407f057b813d494de85c80a6a23f631e7f2bfb2
SheilaWaples <sheilawaples78@gmail.com>
2018-02-23 12:22:05 -0500

    inital commit

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
 
   get 'users/edit'
 
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
