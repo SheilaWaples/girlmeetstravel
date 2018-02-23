e122d426971d374ae8d22bb72d08c9d53c1acf87
SheilaWaples <sheilawaples78@gmail.com>
2018-02-23 12:02:15 -0500

    2nd commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
