5755cdd392bc75b8a1c6dd261e9786c9c2654f15
SheilaWaples <sheilawaples78@gmail.com>
2018-03-29 16:44:06 -0400

    logged in fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,12 @@ class ApplicationController < ActionController::Base
 
 
 	def current_user
-  		@current_user = User.find_by_id(session[:user_id])
+  		@current_user = User.find(session[:user_id])
   	end
+    
     helper_method :current_user, :logged_in?
-   		def logged_in?
+   	
+   	def logged_in?
    		@current_user != nil
- 
-end
+ 	end
 end
